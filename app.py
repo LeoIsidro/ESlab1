8bb52f7396f69a4d1477c66dc90d26f2cad7e048
MarielUTEC <91162593+MarielUTEC@users.noreply.github.com>
2023-08-17 23:19:02 -0500

    Añadiendo las funciones

--- app.py
+++ app.py
@@ -13,3 +13,22 @@ app = Flask(__name__)
 def hello():
 
     return '<h1>Hello, World, we are fine!</h1>'
+
+
+@app.route('/sumar')
+def sumar():
+    param1 = int(request.args.get('param1', 0))
+    param2 = int(request.args.get('param2', 0))
+    result = param1 + param2
+    return f'La suma es: {result}'
+
+@app.route('/multiplicar')
+def multiplicar():
+    param1 = int(request.args.get('param1', 0))
+    param2 = int(request.args.get('param2', 0))
+    result = param1 * param2
+    return f'El producto es: {result}'
+
+
+if __name__ == 'main':
+    app.run()
